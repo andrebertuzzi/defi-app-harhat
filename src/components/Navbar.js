c12a044146ba616134cf3564eaf97d76bb6d5f40
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2021-06-28 18:37:36 -0300

    Fixed html issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,6 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt="" />
           &nbsp; Deco Token Farm
-        </a>
-
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
